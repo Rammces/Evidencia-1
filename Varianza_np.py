fa546c89aeb370444c8f6375783508f18cd37ec8
Rammces <angelcastillosaucedo@gmail.com>
2025-10-02 15:41:32 -0600

    Add files via upload

---
+++ Varianza_np.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+array = np.array([5, 15, 25, 35, 45])
+
+print("Desviación estándar:", np.std(array))
+print("Varianza:", np.var(array))
+print("Suma:", np.sum(array))
+print("Cantidad:", array.size)
